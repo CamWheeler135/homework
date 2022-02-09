eed5d20aa213b3cc11410d26e57a5bdf4bc2c25b
Cameron <cam010100@hotmail.co.uk>
2022-02-09 23:07:13 +0000

    quick save

--- algorithmwork.py
+++ algorithmwork.py
@@ -1,11 +1,24 @@
 
 from gettext import install
 
-
+'''Importing libraries'''
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+'''Importing the dataset'''
 
-print("Hello world")
+dataset = pd.read_csv('Ads_Optimisation.csv')
+
+'''Implementing Random Selection'''
+import random 
+N = 10000
+d = 10
+ads_selected =[]
+total_reward = 0 
+for n in range (0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = total_reward + reward
