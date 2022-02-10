05c370175a42909e1cdbd602d2dcd3a2a610a6f0
Cameron <cam010100@hotmail.co.uk>
2022-02-10 22:55:55 +0000

    addition of matplot lib scatter graph code

--- algorithmwork.py
+++ algorithmwork.py
@@ -1,6 +1,7 @@
 
 from cProfile import run
 from gettext import install
+import matplotlib
 
 '''Importing libraries'''
 
@@ -29,7 +30,7 @@ for x in range (0, 10):
         reward = dataset.values[n, ad]
         total_reward = total_reward + reward
 
-    '''Take and store the total reward, reset total reward, run again '''
+    '''Take and store the total reward, reset total reward, run again'''
 
     graph_random.append(total_reward)
     total_reward = 0
@@ -38,12 +39,24 @@ print(total_reward)
 print (graph_random)
 
 '''Plotting results from 10 runs'''
+run_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-run_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-reward_from_run_y = [graph_random]
-
-plt.plot(run_x, reward_from_run_y)
-
+plt.scatter(run_number, graph_random)
+plt.ylabel("Random Selection Reward")
+plt.xlabel("Run Number")
+plt.title("Random Selection Rewards")
+plt.legend()
 plt.show()
 
+'''Coding epsilon greedy algorithm'''
+
+# choose epsilon expoloration probability 
+# choose n (number of iterations)
+# for i = 1 to n do: 
+    # p = pick a random number from 0 to 1 
+    # if p < epsilone:
+        # current_banit.pull_arm
+
+# reward = current_banit.pull_arm()
+# update estimation for what rewards the curren bandit gives 
 
