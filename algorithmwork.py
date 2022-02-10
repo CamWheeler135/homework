b2ee2a9776c797abefaa813ade97862fa495deb8
Cameron <cam010100@hotmail.co.uk>
2022-02-10 18:49:27 +0000

    store 10 run reward + pyplot attempt

--- algorithmwork.py
+++ algorithmwork.py
@@ -1,4 +1,5 @@
 
+from cProfile import run
 from gettext import install
 
 '''Importing libraries'''
@@ -16,14 +17,33 @@ dataset = pd.read_csv("Ads_Optimisation.csv")
 
 import random 
 N = 10000
-d = 10
+d = 5
 ads_selected =[]
-total_reward = 0 
-for n in range (0, N):
-    ad = random.randrange(d)
-    ads_selected.append(ad)
-    reward = dataset.values[n, ad]
-    total_reward = total_reward + reward
+total_reward = 0
+graph_random = []
 
+for x in range (0, 10):
+    for n in range (0, N):
+        ad = random.randrange(d)
+        ads_selected.append(ad)
+        reward = dataset.values[n, ad]
+        total_reward = total_reward + reward
+
+    '''Take and store the total reward, reset total reward, run again '''
+
+    graph_random.append(total_reward)
+    total_reward = 0
+        
 print(total_reward)
+print (graph_random)
+
+'''Plotting results from 10 runs'''
+
+run_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+reward_from_run_y = [graph_random]
+
+plt.plot(run_x, reward_from_run_y)
+
+plt.show()
+
 
