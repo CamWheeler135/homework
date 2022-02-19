3595fd3d3a0fcb0f82b26e0ca39139c23dbc87a0
Cameron <cam010100@hotmail.co.uk>
2022-02-19 10:49:55 +0000

    working code

--- Fibonacci Sequence.py
+++ Fibonacci Sequence.py
@@ -3,12 +3,13 @@
 # A Fibonacci sequence is the next number in the sequence is the sum of the previous two numbers
 # In maths it is called a recurance relation, meaning that each term of the sequence is a function
 # of the preceding terms 
+# A factorial is the product of all the integers from one to (x) 6 = 1*2*3*4*5*6 = 720
 
-def recur_fibonacci(n):
+def fibonacci_sequence_code(n):
     if n <= 1:
         return n
     else:
-        return(recur_fibonacci(n-1) + recur_fibonacci(n-2))
+        return(fibonacci_sequence_code(n-1) + fibonacci_sequence_code(n-2))
 
 nterms = 10
 
@@ -17,4 +18,4 @@ if nterms <= 0:
 else:
     print("This is the fibonacci sequence:")
     for i in range(nterms):
-        print(recur_fibonacci(i))
+        print(fibonacci_sequence_code(i))
