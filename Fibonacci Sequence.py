57048189cac0a259cfc7ac055b98a92557624957
MattiaCinelli <cinelli.mattia@gmail.com>
2022-03-28 11:18:25 +0100

    adding valiable solution

--- Fibonacci Sequence.py
+++ Fibonacci Sequence.py
@@ -36,7 +36,7 @@ print(every_second_number[::2])'''
 # If the number divides by 5 or contains 5 = chop
 # Change it to a string then we check for symbol if the string contains the symbol then we need to place chip or chop depending on the symbol.
 
-number_list = list(range(1, 160))
+number_list = list(range(1, 160)) # MC range is already a list
 
 '''for i in range(len(number_list)):
     if i % 3 == 0 and i % 5 == 0:
@@ -57,8 +57,8 @@ number_list = list(range(1, 160))
             print(i)'''
 
 for i in (number_list):
-    str(i)
-    if '53' in str(i):
+    str(i) # MC this won't do anything
+    if '53' in str(i): # MC this is called harcoding a solution and hardcoding is bad practice
         print(i, 'ChipChop')
     elif '35' in str(i):
         print(i, 'ChopChip')
@@ -71,8 +71,15 @@ for i in (number_list):
 
 
 
-        
-
-        
-
-
+# print chip if number is devideve by 3 or contains a 3, chop if number is devideve by 5 or contains a 5, chipchop if number is devideve by 3 and 5 or contains a 3 and 5
+n = 310
+for i in range(1, n):
+    out = ""
+    if i % 3 == 0 or '3' in str(i):
+        out += 'chip'
+    if i % 5 == 0 or '5' in str(i):
+        out += 'chop'
+    if (i % 3 == 0 and i % 5 == 0 or '3' in str(i) and '5' in str(i)) and out == 'chipchop':
+        print(i, 'chipchop')
+    else:
+        print(i, out)
